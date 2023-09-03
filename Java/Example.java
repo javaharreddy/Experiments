3920967bff3f599fa4e5493e19f8a2a001c4f698
vemulasaikrishna03 <vemulasaikrishna03@gmail.com>
2023-09-03 22:27:53 +0530

    one file added

---
+++ Java/Example.java
@@ -0,0 +1,17 @@
+package Experiments.Java;
+
+
+
+public class Example {
+    public static void main(String[] args) {
+        // Define two numbers to be added
+        int number1 = 5;
+        int number2 = 7;
+        
+        // Calculate the sum
+        int sum = number1 + number2;
+        
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
