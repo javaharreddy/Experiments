8bd1df81137ef995eea10825b1b4ed9bb31de0e5
vemulasaikrishna03 <vemulasaikrishna03@gmail.com>
2023-09-03 22:33:34 +0530

    added cpp file

---
+++ CPP/example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Define two numbers to be added
+    int number1 = 5;
+    int number2 = 7;
+    
+    // Calculate the sum
+    int sum = number1 + number2;
+    
+    // Print the result
+    std::cout << "The sum of " << number1 << " and " << number2 << " is: " << sum << std::endl;
+    
+    return 0;
+}
