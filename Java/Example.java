7f581902f54995ec627cf2d2c76e46196e557b4b
vemulasaikrishna03 <vemulasaikrishna03@gmail.com>
2023-09-05 10:23:02 +0530

    java updated

--- Java/Example.java
+++ Java/Example.java
@@ -12,6 +12,6 @@ public class Example {
         int sum = number1 + number2;
         
         // Print the result
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+        System.out.println(sum);
     }
 }
