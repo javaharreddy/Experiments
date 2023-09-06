b787c40dfe4d23ce2f696f6ad91b6e2954647da7
Sai Krishna <76392114+vemulasaikrishna03@users.noreply.github.com>
2023-09-06 20:47:13 +0530

    Add files via upload

---
+++ Java/palindrome/palindrome.java
@@ -0,0 +1,22 @@
+public class palindrome{
+public static boolean isPalindrome(String str) {
+        
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+     
+        String reversedStr = new StringBuilder(str).reverse().toString();
+
+        return str.equals(reversedStr);
+
+    }
+
+     public static void main(String[] args) {
+        String input = "racecar"; 
+
+        if (palindrome.isPalindrome(input)) {
+            System.out.println("The entered string is a palindrome.");
+        } else {
+            System.out.println("The entered string is not a palindrome.");
+        }
+    }
+}
