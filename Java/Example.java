f44a991d47773193614050e018623091f9c9d315
vemulasaikrishna03 <vemulasaikrishna03@gmail.com>
2023-09-07 11:29:53 +0530

    java updated

--- Java/Example.java
+++ Java/Example.java
@@ -2,14 +2,14 @@ package Experiments.Java;
 
 public class Example {
     public static void main(String[] args) {
-        // Define two numbers to be added
+     
         int number1 = 5;
         int number2 = 7;
         
-        // Calculate the sum
+   
         int sum = number1 + number2;
         
-        // Print the result
+       
         System.out.println(sum);
     }
 }
